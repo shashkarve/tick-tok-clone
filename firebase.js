59788a876ca9932e5f8d0cb3d6e009c07c5a2c99
Shashank Karve <shashkarve@gmail.com>
2023-02-19 22:52:42 -0500

    feat: first commit with basic setup

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA4USoUhpmg7HpqIl6p57VnEdOnydQgwcU",
+    authDomain: "tik-tok-clone-4f8f7.firebaseapp.com",
+    projectId: "tik-tok-clone-4f8f7",
+    storageBucket: "tik-tok-clone-4f8f7.appspot.com",
+    messagingSenderId: "905542649047",
+    appId: "1:905542649047:web:8ce1bf76658b678d1adc92",
+    measurementId: "G-Y297CYJDJK"
+  };
